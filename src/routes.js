fe3392a146fbc7e2d33d6c07caf8a0855be6fc21
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2020-01-15 23:22:58 -0300

    create Devs with controllers, Point with coordinates and integrating on github

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,8 @@
 const { Router } = require('express');
-const axios = require('axios');
-const Dev = require('./models/Dev');
+const DevController = require('./controllers/DevControllers');
 
 const routes = Router();
 
-routes.post('/devs', async (req, res) => {
-  const { github_username, techs } = req.body
-  console.log(github_username)
-  const apiRes = await axios.get(`https://api.github.com/users/${github_username}`)
-
-  const { name = login, avatar_url, bio } = apiRes.body
-  
-  const techsArray = techs.split(',').map(tech => tech.trim())
-
-  return res.json({test: 'hello'})
-})
+routes.post('/devs', DevController.store)
 
 module.exports = routes;
