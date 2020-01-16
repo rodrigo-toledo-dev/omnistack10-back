d2d5b9d3c46b4764f48fbdeacd0ee5739443bb95
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2020-01-15 23:29:12 -0300

    list devs in controller using routes

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@ const DevController = require('./controllers/DevControllers');
 
 const routes = Router();
 
+routes.get('/devs', DevController.index)
 routes.post('/devs', DevController.store)
+
 
 module.exports = routes;
